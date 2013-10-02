86f8decce4f761e2f58b373776afd194dfb9ad5b
oliverjatwal <oliverjatwal@gmail.com>
2013-10-02 20:00:49 +0100

    lots more flakes

--- resources/snowstorm.js
+++ resources/snowstorm.js
@@ -15,7 +15,7 @@ var snowStorm = (function(window, document) {
   // --- common properties ---
 
   this.autoStart = true;          // Whether the snow should start automatically or not.
-  this.flakesMax = 128;           // Limit total amount of snow made (falling + sticking)
+  this.flakesMax = 256;           // Limit total amount of snow made (falling + sticking)
   this.flakesMaxActive = 64;      // Limit amount of snow falling at once (less = lower CPU use)
   this.animationInterval = 33;    // Theoretical "miliseconds per frame" measurement. 20 = fast + smooth, but high CPU use. 50 = more conservative, but slower
   this.excludeMobile = false;      // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) By default, be nice.
